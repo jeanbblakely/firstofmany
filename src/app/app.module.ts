b1f4baa4dde982873c166712ec148b40589a0d67
Jeannie Blakely <jeanbblakely@gmail.com>
2019-06-11 11:43:13 -0400

    basic structure setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './experiences/categories/categories.component';
+import { DashboardComponent } from './user/dashboard/dashboard.component';
+import { AccountComponent } from './user/account/account.component';
+import { LoginComponent } from './user/login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    DashboardComponent,
+    AccountComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
