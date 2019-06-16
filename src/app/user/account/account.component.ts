6f76af8b7004a6a5f85c7b3d2b244c5dc985e716
Jeannie Blakely <jeanbblakely@gmail.com>
2019-06-16 15:34:09 -0400

    moved account to reactive form; updated Materials version w/ ng update @angular/material@latest

--- src/app/user/account/account.component.ts
+++ src/app/user/account/account.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-account',
@@ -9,10 +12,23 @@ export class AccountComponent implements OnInit {
   public username: string = '';
   public password: string = '';
   public message: string = '';
+  public userForm: FormGroup = new FormGroup({
+    username: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+    email: new FormControl(null, Validators.required),
+    name: new FormControl(null, Validators.required),
+    birthdate: new FormControl(null, Validators.required),
+    gender: new FormControl(null, Validators.required)
+  });
 
-  constructor() { }
+  constructor(private userService: UserService, 
+    private router: Router) { }
 
   ngOnInit() {
+  }
+  
+  register() {
+    console.log('User Control Value', this.userForm.value);
   }
 
 }
