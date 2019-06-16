91dcdcb301d63035fb525e34a0da2b7d5d6329e2
Joey Marinelli <jmarine1@my.westga.edu>
2019-06-15 23:07:48 -0500

    Added DB access to the category model

--- backend/models/experience.js
+++ backend/models/experience.js
@@ -2,7 +2,5 @@ var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Experience', {
     name: String,
-    note: String,
-    img: File,
-    timestamp: true
+    note: String
 })
