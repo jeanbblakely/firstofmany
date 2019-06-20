97b18b48da44aba1810ccdd99b6289526c276d8a
Joey Marinelli <jmarine1@my.westga.edu>
2019-06-19 22:27:11 -0500

    removed posts route and messages array

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,13 +7,6 @@ export class ApiService {
   constructor(private httpClient: HttpClient,
     private router: Router) {}
 
-  messages = []
-  getMessage() {
-    this.httpClient.get('http://localhost:3000/posts').subscribe(res =>{
-      console.log(res);
-    });
-  }
-
   sendUserRegistration(regData) {
     this.httpClient.post('http://localhost:3000/account', regData).subscribe(res =>{
         console.log(res);
