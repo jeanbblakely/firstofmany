439719a0b979d1ed5a78a2ca935706f306115cee
Jeannie Blakely <jeanbblakely@gmail.com>
2019-06-20 16:09:12 -0400

    added register component functionality

--- src/app/user/register/register.component.spec.ts
+++ src/app/user/register/register.component.spec.ts
@@ -1,4 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { ApiService } from './../../api.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './../../material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 import { RegisterComponent } from './register.component';
 
@@ -8,6 +16,18 @@ describe('RegisterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ 
+        FormsModule, 
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        MaterialModule
+      ],
+      providers: [ 
+        { provide: Router, useClass: class { navigate = jasmine.createSpy("navigate"); } },
+        UserService,
+        { provide: UserService }
+      ],
       declarations: [ RegisterComponent ]
     })
     .compileComponents();
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+import { User } from './../../models/user';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  public message: string = '';
+  public userForm: FormGroup;
 
-  constructor() { }
+  constructor(private userService: UserService,
+    private router: Router, private fb: FormBuilder) {
+      this.createForm();
+    }
+
 
   ngOnInit() {
+  }
+  
+      /*
+	Creates userForm based on input
+  */
+  createForm() {
+    this.userForm = this.fb.group({
+      username: [null, Validators.required],
+      password: [null, Validators.required],
+      email: [null, Validators.required],
+      name: [null, Validators.required],
+      birthdate: [null, Validators.required],
+      gender: [null, Validators.required]
+    });
+  }
+
+  /*
+	Registers user in the system. Routes to Login page
+  */
+  register() {
+    console.log('User Control Value', this.userForm.value);
+    const result: User = Object.assign({}, this.userForm.value);
+    console.log('after copy', result);
+    if (this.userService.register(result)) {
+      console.log('Successfully registered');
+      this.message = 'thanks for registering';
+      this.router.navigate(['login']);
+    } else {
+      this.message = 'error in registering';
+    }
   }
 
 }
