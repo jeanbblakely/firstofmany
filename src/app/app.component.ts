7d415deb79bb26123898767d597faeea4897dc40
Jeannie Blakely <jeanbblakely@gmail.com>
2019-06-20 12:53:56 -0400

    added user service and isLoggedIn method; no way for header to access

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from './api.service';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,16 @@ import { ApiService } from './api.service';
 export class AppComponent {
   title = 'the First of Many';
 
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService, private userService: UserService) {}
 
   ngOnInit() {
     this.apiService.getMessage();
+  }
+  
+     /*
+	Checks to see if User is signed in (ie instantiated user.id)
+  */
+  isLoggedIn(): boolean {
+    return this.userService.isLoggedIn();
   }
 }
