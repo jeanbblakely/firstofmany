bff7d8b7caf3a895b1c16c9f457b9ffeaac23605
James Luke Johnson <jjohn144@my.westga.edu>
2019-06-24 02:41:16 -0500

    Materialized registration form.
    Used validation and responsive layouts.

---
+++ src/app/_interface/User.model.ts
@@ -0,0 +1,8 @@
+export interface User {
+  username: string;
+  password: string;
+  name: string;
+  email: string;
+  birthdate: Date;
+  gender: string;
+}
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
 import { User } from './../../models/user';
+import { NewUser } from '../../_interface/User.model';
 
 @Component({
   selector: 'app-register',
@@ -10,31 +11,63 @@ import { User } from './../../models/user';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  public message: string = '';
-  public userForm: FormGroup;
+  message = '';
+  userForm: FormGroup;
+  genders = ['--', 'Female', 'Male', 'Non-binary/third gender', 'Prefer not to say'];
+  minDate = new Date(1900,0,1);
+  maxDate = new Date(Date.now() + 24 * 60 * 60 * 1000);
 
-  constructor(private userService: UserService,
-    private router: Router, private fb: FormBuilder) {
-      this.createForm();
+  constructor(private userService: UserService, private fb: FormBuilder,
+    private router: Router) {
     }
 
-
   ngOnInit() {
+    this.createForm();
   }
   
-      /*
+  /*
 	Creates userForm based on input
   */
-  createForm() {
+  private createForm() {
     this.userForm = this.fb.group({
-      username: [null, Validators.required],
-      password: [null, [Validators.required, Validators.minLength(8)]],
-      email: [null, Validators.required],
-      name: [null, Validators.required],
-      birthdate: [null, Validators.required],
-      gender: [null, Validators.required]
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      birthdate: [new Date(), [Validators.required]],
+      gender: [this.genders[0], [Validators.required, Validators.pattern('((?!--).)*')]],
     });
   }
+
+  public hasError = (controlName: string, errorName: string) => {
+    return this.userForm.controls[controlName].hasError(errorName);
+  }
+
+  // Great guide at https://code-maze.com/angular-material-form-validation/ which we may want to follow
+  // public createUser = (userFormValue) => {
+  //   if (this.userForm.valid) {
+  //     this.executeUserCreation(userFormValue);
+  //   }
+  // }
+
+  // private executeUserCreation = (userFormValue) => {
+  //   let user: NewUser = {
+  //     username: userFormValue.username,
+  //     password: userFormValue.password,
+  //     name: userFormValue.name,
+  //     email: userFormValue.email,
+  //     birthdate: userFormValue.birthdate,
+  //     gender: userFormValue.gender
+  //   }
+  //   this.userService.register(user)
+  //     .subscribe(res => {
+  //       this.message = 'thanks for registering';
+  //       this.router.navigate(['login']);
+  //     },
+  //     (error => {
+  //       this.message = 'error in registering';
+  //     }))
+  // }
 
   /*
 	Registers user in the system. Routes to Login page
