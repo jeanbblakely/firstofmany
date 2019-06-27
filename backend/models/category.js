a5802908cfd1e90eadad23124815203f917afbba
Joey Marinelli <jmarine1@my.westga.edu>
2019-06-27 14:04:24 -0400

    Changed user and category schema to properly store category and experience objects

--- backend/models/category.js
+++ backend/models/category.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var categorySchema = new mongoose.Schema({
     name: String,
-    experiences: [{mongoose.Schema.Types.ObjectId, ref: 'Experience'}];
+    experiences: { type: [mongoose.Schema.Types.ObjectId], ref: 'Experience'}
 })
 
 module.exports = mongoose.model('Category', categorySchema);
--- backend/models/user.js
+++ backend/models/user.js
@@ -8,7 +8,7 @@ var userSchema = new mongoose.Schema({
   name: String,
   birthdate: Date,
   gender: String,
-  tracked_categories: [{mongoose.Schema.Types.ObjectId, ref: 'Category'}],
+  tracked_categories: { type: [mongoose.Schema.Types.ObjectId], ref: 'Category'},
   favorites: Array
 })
 
