f19296f28c9b532e752604e63425c29059db2cd2
James Luke Johnson <jjohn144@my.westga.edu>
2019-06-27 16:58:45 -0500

    Added snackbar to app.component.
    Added watcher on UserService on logout$.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserService } from './services/user.service';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,12 @@ import { UserService } from './services/user.service';
 export class AppComponent {
   title = 'the First of Many';
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private snackBar: MatSnackBar) {
+    userService.logout$.subscribe(() => {
+        this.snackBar.open('You are logged out', 'Okay', {duration: 2000});
+      }
+    )
+  }
 
   ngOnInit() {
   }
