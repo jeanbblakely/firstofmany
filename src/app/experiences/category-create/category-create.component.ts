6beeb7427e9c5962fac88aaf925ba78b8a93dfaf
Jeannie Blakely <jeanbblakely@gmail.com>
2019-07-01 22:25:48 -0400

    used formarray for experiences; updated form

--- src/app/experiences/category-create/category-create.component.ts
+++ src/app/experiences/category-create/category-create.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl, FormArray } from '@angular/forms';
 import { CategoryService } from '../../services/category.service';
 import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
 import { Observable, of, throwError, Subject } from 'rxjs';
 import { Category } from './../../models/category';
+import { User } from './../../models/user';
 
 @Component({
   selector: 'app-category-create',
@@ -14,11 +15,13 @@ import { Category } from './../../models/category';
 export class CategoryCreateComponent implements OnInit {
   message = '';
   categoryForm: FormGroup;
+  user: User;
 
   constructor(private categoryService: CategoryService, private userService: UserService, private fb: FormBuilder,
     private router: Router) { }
 
   ngOnInit() {
+    this.getUser();
     this.createForm();
   }
   
@@ -27,9 +30,33 @@ export class CategoryCreateComponent implements OnInit {
   */
   private createForm() {
     this.categoryForm = this.fb.group({
-      name: ['', [Validators.required]]
+      name: ['', [Validators.required]],
+      experiences: this.fb.array([])
     });
   }
+  
+  get experiences(): FormArray {
+    return this.categoryForm.get('experiences') as FormArray;
+  }
+  
+  addExperience() {
+    this.experiences.push(this.fb.group({
+      name: ['', [Validators.required]],
+      note: ['', [Validators.required]],
+      favorite: false
+    }))
+  }
+  
+  /*
+	Gets Observable User from service and fills in form
+  */
+  getUser() {
+    this.userService.getUser().subscribe(data => {
+      this.user = data;
+    });
+
+  }
+
   
   public hasError = (controlName: string, errorName: string) => {
     return this.categoryForm.controls[controlName].hasError(errorName);
@@ -48,10 +75,6 @@ export class CategoryCreateComponent implements OnInit {
       } else {
         var currentCategory = this.searchCategory(this.categoryForm.get('name').value);
         console.log(currentCategory, 'current category');
-        var currentUser;
-        this.userService.getUser()
-          .subscribe(currentUser => currentUser);
-        console.log(currentUser, 'current user');
         //todo updateUser with Category in UserService
       }
     } else {
