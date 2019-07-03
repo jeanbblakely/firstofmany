6d7fb749096e029957d1af6e1fd417ef2a0dabbf
James Luke Johnson <jjohn144@my.westga.edu>
2019-07-03 11:12:53 -0500

    Modified spacebreak pipe to split long words. There's an optional maxChars parameter now. Tested and working!

--- src/app/_pipes/spacebreak.pipe.spec.ts
+++ src/app/_pipes/spacebreak.pipe.spec.ts
@@ -6,18 +6,23 @@ describe('SpacebreakPipe', () => {
     expect(pipe).toBeTruthy();
   });
 
-  it('separate a string into line breaks at each space', () => {
+  it('should separate a string into line breaks at each space', () => {
     const pipe = new SpacebreakPipe();
     expect(pipe.transform('this is the song that never ends')).toEqual('this\nis\nthe\nsong\nthat\nnever\nends');
   });
 
-  it('replace multiple spaces with a single line break', () => {
+  it('should replace multiple spaces with a single line break', () => {
     const pipe = new SpacebreakPipe();
     expect(pipe.transform('hello   world')).toEqual('hello\nworld');
   });
 
-  it('remove whitespace around the word before adding line breaks', () => {
+  it('should remove whitespace around the word before adding line breaks', () => {
     const pipe = new SpacebreakPipe();
     expect(pipe.transform('    hi     ')).toEqual('hi');
+  });
+
+  it('should split long words at the optional parameter maxChars', () => {
+    const pipe = new SpacebreakPipe();
+    expect(pipe.transform('thisis22characterslong', 10)).toEqual('thisis22ch\naracterslo\nng');
   });
 });
