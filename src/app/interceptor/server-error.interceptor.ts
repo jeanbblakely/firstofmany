ed1715916ad28638bfbb91af6bc0294bc42bb411
Jeannie Blakely <jeanbblakely@gmail.com>
2019-07-12 21:22:13 -0400

    added snackbar to injector; displaying db errors

--- src/app/interceptor/server-error.interceptor.ts
+++ src/app/interceptor/server-error.interceptor.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+
 import { 
   HttpEvent, HttpRequest, HttpHandler,
   HttpInterceptor, HttpErrorResponse
@@ -9,12 +11,16 @@ import { retry, catchError } from 'rxjs/operators';
 @Injectable()
 
 export class ServerErrorInterceptor implements HttpInterceptor {
+  constructor(private snackBar: MatSnackBar) {}
+
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       retry(1),
       catchError((error: HttpErrorResponse) => {
         console.log('Error Intercepted');
         console.log(error.error.message, 'error');
+        this.snackBar.open(error.error.message, 'Okay');
         return throwError(error);
       })
     );
