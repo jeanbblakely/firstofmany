c9c610111a3f9fbea2c7d569c0f385b89c72dcbc
James Luke Johnson <jjohn144@my.westga.edu>
2019-07-18 00:41:58 -0500

    Added sorted, filterable table to AddCategory.

--- src/app/experiences/add-categories/add-categories.component.ts
+++ src/app/experiences/add-categories/add-categories.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Component, Inject, ViewChild, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA, MatTableDataSource, MatSort } from '@angular/material';
 import { Category } from 'src/app/models/category';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-add-categories',
@@ -9,15 +10,47 @@ import { Category } from 'src/app/models/category';
 })
 export class AddCategoriesComponent implements OnInit {
   categories: Category[];
+  displayedColumns: string[] = ['select', 'name'];
+  dataSource: MatTableDataSource<Category>;
+  selection = new SelectionModel<Category>(true, []);
+  
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private dialogRef: MatDialogRef<AddCategoriesComponent>, @Inject(MAT_DIALOG_DATA) data) { 
     this.categories = data;
+    this.dataSource = new MatTableDataSource(this.categories);
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.dataSource.sort = this.sort;
   }
 
-  
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: Category): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.name}`;
+  }
+
   /**
    * Closes the dialog
    */
