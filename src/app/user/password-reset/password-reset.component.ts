42bb9eab0acdc61bd96492a26218d9407e7e9700
Jeannie Blakely <jeanbblakely@gmail.com>
2019-07-18 16:52:53 -0400

    basic reset password page

--- src/app/user/password-reset/password-reset.component.ts
+++ src/app/user/password-reset/password-reset.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-password-reset',
   templateUrl: './password-reset.component.html',
   styleUrls: ['./password-reset.component.css']
 })
-export class PasswordResetComponent implements OnInit {
+export class PasswordResetComponent {
+  public username = '';
+  public security_question = 'this';
+  public resetData: FormGroup;
 
-  constructor() { }
+  public message: string = '';
+  constructor(private userService: UserService,
+    private router: Router, private fb: FormBuilder) {
+      this.createForm();
+    }
 
-  ngOnInit() {
+ /*
+	Checks form for errors
+  */
+  public hasError = (controlName: string, errorName: string) => {
+    return this.resetData.controls[controlName].hasError(errorName);
+  }
+
+   /*
+	Creates resetData based on input
+  */
+  private createForm() {
+    this.resetData = this.fb.group({
+      username: [null, [Validators.required]],
+      security_answer: [null, [Validators.required]],
+    });
   }
 
 }
