2ddf80395073e3007df8aafa95157f1fcb26d6b7
James Luke Johnson <jjohn144@my.westga.edu>
2019-07-20 14:32:39 -0500

    Wrote the tour.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserService } from './services/user.service';
 import { MatSnackBar } from '@angular/material';
+import { TourService } from 'ngx-tour-md-menu';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,67 @@ import { MatSnackBar } from '@angular/material';
 export class AppComponent {
   title = 'the First of Many';
 
-  constructor(private userService: UserService, private snackBar: MatSnackBar) {
+  constructor(private userService: UserService, private snackBar: MatSnackBar, public tourService: TourService) {
     userService.logout$.subscribe(() => {
         this.snackBar.open('You are logged out', 'Okay', {duration: 2000});
       }
     )
+    this.tourService.initialize([
+      {
+        anchorId: 'start-tour',
+        content: 'Thanks for registering and welcome to First of Many!',
+        placement: 'center',
+        title: 'Welcome',
+      }, {
+        anchorId: 'your-categories',
+        content: "First of Many is an app to track all of life's experiences!\nHere are all the categories want to keep track of.",
+        title: 'Your categories',
+      }, {
+        anchorId: 'add-categories',
+        content: "To start tracking your first experiences, you first need to add some categories.",
+        title: 'Add categories',
+        enableBackdrop: false,
+      }, {
+        anchorId: 'add-categories-start',
+        content: "Anything you experience for the first time, can be tracked! Here are a few suggestions.",
+        placement: 'center',
+        enableBackdrop: false,
+        title: 'Add categories'
+      }, {
+        anchorId: 'add-categories-search',
+        content: "You can search for categories here.",
+        title: 'Search categories',
+      }, {
+        anchorId: 'add-categories-create',
+        content: "If you don't find what you're looking for, you can create a new category, too!",
+        title: 'Create categories',
+      }, {
+        anchorId: 'add-some-categories',
+        content: "Go ahead and add some categories.\nWe'll continue to tour when you're finished.",
+        enableBackdrop: false,
+        title: 'Add some categories',
+      }, {
+        anchorId: 'user-categories',
+        content: "Now that you have some categories, let's add an experience.\nClick a category.",
+        title: 'Your categories',
+        enableBackdrop: false,
+        route: '/dashboard',
+      }, {
+        anchorId: 'add-experiences',
+        content: "Here are all of your first (of many) experiences. You don't have any yet.\nClick here to add your first experience.",
+        title: 'Your first experience',
+        enableBackdrop: false,
+        route: '/dashboard',
+      }, {
+        anchorId: 'all-done',
+        content: "Thanks for taking the tour! We hope you enjoy your first of many visits to this app!",
+        title: 'Thank you!',
+        route: '/dashboard',
+      }], {
+        enableBackdrop: true,
+        route: '/dashboard',
+      });
+    this.tourService.start();
   }
 
   ngOnInit() {}
